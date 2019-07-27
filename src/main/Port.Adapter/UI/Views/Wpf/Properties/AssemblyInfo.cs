744995b3a74a09cc6ade6b51a62aea1ace69359c
Elmer Roderick Bool <elmer.bool@ei8.works>
2019-07-27 13:03:10 +1000

    Update AssemblyInfo.cs

--- src/main/Port.Adapter/UI/Views/Wpf/Properties/AssemblyInfo.cs
+++ src/main/Port.Adapter/UI/Views/Wpf/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("d# NeurUL Studio")]
+[assembly: AssemblyTitle("d# neurUL Studio")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ei8")]
-[assembly: AssemblyProduct("Dasz")]
+[assembly: AssemblyProduct("d# neurUL Studio")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
