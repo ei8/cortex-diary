c13fac5087e7b33ad0622e881605fb2a069c44f5
Elmer Roderick Bool <elmer.bool@ei8.works>
2019-11-02 10:20:49 +1100

    Added EditorBox.

---
+++ src/main/Port.Adapter/UI/Views/Blazor/Data/EditorModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace works.ei8.Cortex.Diary.Port.Adapter.UI.Views.Blazor.Data
+{
+    public class EditorModel
+    {
+        public string Tag { get; set; }
+    }
+}
