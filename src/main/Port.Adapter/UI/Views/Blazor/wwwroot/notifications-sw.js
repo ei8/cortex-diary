867b5088624247a6abcd41f5a934921c2d762327
Junvic <contact@junvic.me>
2022-07-30 21:51:38 +0800

    #19: integrate push notifications API calls

---
+++ src/main/Port.Adapter/UI/Views/Blazor/wwwroot/notifications-sw.js
@@ -0,0 +1,28 @@
+﻿self.addEventListener('push', function (event) {
+    if (!(self.Notification && self.Notification.permission === 'granted')) {
+        return;
+    }
+
+    let data = {};
+
+    if (event.data) {
+        data = event.data.json();
+    }
+
+    event.waitUntil(self.registration.showNotification(data.title, data.options));
+});
+
+self.addEventListener('notificationclick', function (event) {
+    event.notification.close();
+
+    const tag = event.notification.tag;
+    const targetUrl = `/landing/${tag}`;
+
+    event.waitUntil(clients.matchAll({
+        type: "window"
+    }).then(function (clientList) {
+        // open a new window/tab to the target URL
+        if (clients.openWindow)
+            return clients.openWindow(targetUrl);
+    }));
+});
