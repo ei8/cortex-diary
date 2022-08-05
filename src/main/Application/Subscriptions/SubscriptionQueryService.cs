ad70b0fa1f6e8fc3bbb81e299b9c489654878029
abdo samara <abdoo.ahmed38@icloud.com>
2022-08-05 14:28:16 +0200

    Squashed commit of the following:
    commit 73362f3981d3a8063818c8d6e8a011d10a79b8c0
    Author: abdo samara <abdoo.ahmed38@icloud.com>
    Date:   Fri Aug 5 14:25:11 2022 +0200
    
        Squashed commit of the following:
    
        commit 8327716d1d39753b397f237edfefae6c9c55f1ec
        Author: elmerbool <35117496+elmerbool@users.noreply.github.com>
        Date:   Thu Aug 4 22:26:55 2022 +1000
    
            Updated nucleus client and image version.
    
        commit f5cadf496dc40966ddf817068db154a87cb378d2
        Author: Junvic <instilledbee@yahoo.com.ph>
        Date:   Tue Aug 2 19:32:04 2022 +0800
    
            #19: integrate push notifications API calls (#55)

---
+++ src/main/Application/Subscriptions/SubscriptionQueryService.cs
@@ -0,0 +1,28 @@
+﻿using ei8.Cortex.Diary.Application.Settings;
+using ei8.Cortex.Diary.Nucleus.Client.Out;
+using ei8.Cortex.Subscriptions.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ei8.Cortex.Diary.Application.Subscriptions
+{
+    public class SubscriptionQueryService : ISubscriptionQueryService
+    {
+        private readonly ISubscriptionConfigurationClient client;
+        private readonly ISettingsService settings;
+
+        public SubscriptionQueryService(ISubscriptionConfigurationClient client, ISettingsService settings)
+        {
+            this.client = client;
+            this.settings = settings;
+        }
+
+        public async Task<SubscriptionConfiguration> GetServerConfigurationAsync(string avatarUrl, CancellationToken token = default)
+        {
+            return await this.client.GetServerConfigurationAsync(avatarUrl);
+        }
+    }
+}
