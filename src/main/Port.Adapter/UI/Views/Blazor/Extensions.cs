4c5272b9bb6be1bb057b783c869b92ef44662fd8
elmerbool <35117496+elmerbool@users.noreply.github.com>
2022-11-30 23:52:19 +1100

    Refactored Sidebar changes.

---
+++ src/main/Port.Adapter/UI/Views/Blazor/Extensions.cs
@@ -0,0 +1,35 @@
+﻿using ei8.Cortex.Diary.Domain.Model;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ei8.Cortex.Diary.Port.Adapter.UI.Views.Blazor
+{
+    public static class Extensions
+    {
+        public static bool HasActiveChild(this View value, IEnumerable<View> views, string currentUrl)
+        {
+            bool result = false;
+
+            var currentView = views.SingleOrDefault(v => v.Url == currentUrl);
+            while (currentView != null)
+            {
+                // get parent of currentview
+                currentView = views.SingleOrDefault(v => v.Url == currentView.ParentUrl);
+
+                if (value == currentView)
+                {
+                    result = true;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        public static RenderFragment GenerateIconName(this View value) =>
+            new RenderFragment(b => b.AddMarkupContent(0, $"<span class=\"oi {value.Icon}\" aria-hidden=\"true\"></span>"));
+    }
+}
