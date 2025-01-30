8c7cd8ce4117b9b9b446ba899546eeaccc159931
elmerbool <35117496+elmerbool@users.noreply.github.com>
2025-01-30 21:16:23 +1100

    Squashed commit of the following:
    commit a94785c4f64508946fc5253b9b8088d9bdcc1126
    Author: Abdelrhman Ahmed <64210458+AbdulrahmanAhmeed@users.noreply.github.com>
    Date:   Mon Jan 27 23:49:13 2025 +0200
    
        Rename IsExpandModalVisible to IsVisible and OnCancelExpand to OnCancelProcess
    
    commit c4fd237939026c796aaeee49a270dcfa380f24ea
    Author: Abdelrhman Ahmed <64210458+AbdulrahmanAhmeed@users.noreply.github.com>
    Date:   Sat Jan 25 12:06:57 2025 +0200
    
        Implement elmer's final comments
    
    commit 93d3ad5b932f3fd7e03d3251e7dc173fc3f459af
    Author: Abdelrhman Ahmed <64210458+AbdulrahmanAhmeed@users.noreply.github.com>
    Date:   Fri Jan 24 14:11:17 2025 +0200
    
        Encapsulate TreeNeuronViewModel Timer
    
    commit 9443b8864f3f6eda1e8daf109ebaf476eef7e981
    Author: Abdelrhman Ahmed <64210458+AbdulrahmanAhmeed@users.noreply.github.com>
    Date:   Thu Jan 16 20:27:55 2025 +0200
    
        Implement Process Box
    
    commit 8808fcf903b014a109bcceccb3350a23655e85e4
    Author: Abdelrhman Ahmed <64210458+AbdulrahmanAhmeed@users.noreply.github.com>
    Date:   Wed Jan 8 19:45:03 2025 +0200
    
        Fix TreeNeuronViewModel issues
    
    commit 31e1e629b50f9166d6d14e5cb3ff343b2ed5d740
    Merge: 5575648 13bcbd0
    Author: Abdelrhman Ahmed <64210458+AbdulrahmanAhmeed@users.noreply.github.com>
    Date:   Tue Jan 7 10:46:23 2025 +0200
    
        Merge branch 'pr/85-review' into master2
    
    commit 55756483a297be708fa5d2ce918ef75dbcf86d78
    Author: Abdelrhman Ahmed <64210458+AbdulrahmanAhmeed@users.noreply.github.com>
    Date:   Fri Jan 3 16:13:21 2025 +0200
    
        Implement_StartExpandPostsynapticsUntilExternalReferences_Method
    
    commit 13bcbd016279fc54aa32526cb036b8a0775ef91f
    Author: elmerbool <35117496+elmerbool@users.noreply.github.com>
    Date:   Fri Dec 27 08:26:12 2024 +1100
    
        Review changes.
    
    commit c1397b30c8f5654f6dca6799633a1ff29efd65c2
    Author: Abdelrhman Ahmed <64210458+AbdulrahmanAhmeed@users.noreply.github.com>
    Date:   Sat Dec 21 09:26:35 2024 +0200
    
        ExpandUntillPostsynapticExteralReferences_Master

--- src/main/Port.Adapter/UI/ViewModels/TreeNeuronViewModel.cs
+++ src/main/Port.Adapter/UI/ViewModels/TreeNeuronViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using ei8.Cortex.Diary.Application.Neurons;
 using ei8.Cortex.Library.Client;
 using ei8.Cortex.Library.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace ei8.Cortex.Diary.Port.Adapter.UI.ViewModels
 {
@@ -11,6 +13,7 @@ namespace ei8.Cortex.Diary.Port.Adapter.UI.ViewModels
     {
         private string avatarUrl;
         private INeuronQueryService neuronQueryService;
+        private Timer expandPostsynapticsUntilExternalReferencesTimer;
 
         public TreeNeuronViewModel(Neuron neuron, string avatarUrl, INeuronQueryService neuronQueryService)
         {
@@ -18,6 +21,7 @@ namespace ei8.Cortex.Diary.Port.Adapter.UI.ViewModels
             this.avatarUrl = avatarUrl;
             this.neuronQueryService = neuronQueryService;
             this.Children = new List<TreeNeuronViewModel>();
+            this.expandPostsynapticsUntilExternalReferencesTimer = new Timer();
         }
 
         public IList<TreeNeuronViewModel> Children { get; set; }
@@ -44,6 +48,34 @@ namespace ei8.Cortex.Diary.Port.Adapter.UI.ViewModels
                 }
                 this.ExpansionState = ExpansionState.Expanded;
             }
+        }
+
+        public void ConfigureExpandTimer(double interval, ElapsedEventHandler handler)
+        {
+            this.expandPostsynapticsUntilExternalReferencesTimer.Interval = interval;
+            this.expandPostsynapticsUntilExternalReferencesTimer.Elapsed -= handler;
+            this.expandPostsynapticsUntilExternalReferencesTimer.Elapsed += handler;
+        }
+
+        public void StartExpandTimer()
+        {
+            this.expandPostsynapticsUntilExternalReferencesTimer.Start();
+        }
+
+        public void StopExpandTimer()
+        {
+            this.expandPostsynapticsUntilExternalReferencesTimer.Stop();
+        }
+
+        public void RestartExpandTimer()
+        {
+            this.expandPostsynapticsUntilExternalReferencesTimer.Enabled = false;
+            this.StartExpandTimer();
+        }
+
+        public bool IsExpandTimerEnabled()
+        {
+            return this.expandPostsynapticsUntilExternalReferencesTimer.Enabled;
         }
     }
 }
