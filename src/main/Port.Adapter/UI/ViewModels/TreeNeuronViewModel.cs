cf1a142659600683f17a8e3e5effed0f7a4ba33f
Abdelrhman Ahmed <64210458+AbdulrahmanAhmeed@users.noreply.github.com>
2025-02-26 17:24:27 +0200

    Revert Elmer requested changes

--- src/main/Port.Adapter/UI/ViewModels/TreeNeuronViewModel.cs
+++ src/main/Port.Adapter/UI/ViewModels/TreeNeuronViewModel.cs
@@ -39,9 +39,8 @@ namespace ei8.Cortex.Diary.Port.Adapter.UI.ViewModels
                 var children = new List<TreeNeuronViewModel>();
                 if (Library.Client.QueryUrl.TryParse(this.avatarUrl, out QueryUrl result))
                 {
-                    int queryStart = this.avatarUrl.IndexOf('?');
-                    string queryString = this.avatarUrl.Substring(queryStart);
-                    NeuronQuery.TryParse(queryString, out NeuronQuery query);
+                    
+                    NeuronQuery.TryParse(result.QueryString, out NeuronQuery query);
                     var childrenQuery = new NeuronQuery() {
                         PageSize = Constants.TreeNodeChildrenQueryPageSize,
                         RelativeValues = query.RelativeValues
